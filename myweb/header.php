0ce6c2208b1838c14aae67c35a4b1348629c9d49
Relic2904 <shitsamarelic@gmail.com>
2024-07-19 14:00:05 +0300

    Add files via upload

---
+++ myweb/header.php
@@ -0,0 +1,9 @@
+<header>
+<h1>My Web aplication</h1>
+</header>
+<nav>
+    
+<a href="#">Home</a>
+<a href="#">About</a>
+<a href="#">contact</a>
+</nav>
---
+++ myweb/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+< lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport"content="width=device-width,initial-scale=1.0">
+<title>About-My Web Application</title>
+<link rel="stylesheet"href="styles.css">
+</head>
+    <?php
+    include "header.php"
+    ?>
+<div class="container">
+<h2>About Us</h2>
+<p>This page provides information about our web application.</p>
+</div>
+<a href="contact.html">click here to go tocontact</a>
+</body>
+</html>
